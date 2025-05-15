a25f5236e1daab5fc2d69952d4b52c610df93244
Dilip Malani <144886887+techyz-the-tech-people@users.noreply.github.com>
2025-05-15 23:06:04 +0530

    Delete 06 - 2025-05-12 - Day Six - Spring Framework I directory

--- 06 - 2025-05-12 - Day Six - Spring Framework I/src/main/java/com/zeta/spring/ioc/bean/Product.java
+++
@@ -1,8 +0,0 @@
-package com.zeta.spring.ioc.bean;
-
-public class Product {
-    private String name;
-
-    public void setName(String name) { this.name = name; }
-    public String getName() {return this.name; }
-}
--- 06 - 2025-05-12 - Day Six - Spring Framework I/src/main/java/com/zeta/spring/ioc/config/AppConfig.java
+++
@@ -1,6 +0,0 @@
-package com.zeta.spring.ioc.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration @ComponentScan("com.zeta.spring.ioc.bean") public class AppConfig { }
